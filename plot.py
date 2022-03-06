c842a6e874573a76857535bb3e0b8d91419fb4fe
ShrawakB <sb02431318@student.ku.edu.np>
2022-03-06 19:06:03 +0545

    Slight improvement

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import time
 
 _, (plt1, plt2) = plt.subplots(nrows=1, ncols=2)
-sizes = range(100,10000,100)
+sizes = range(100,100000,100)
 linear_besttime = []
 linear_worsttime = []
 binary_besttime = []
@@ -29,7 +29,7 @@ for size in sizes:
 plt1.plot(sizes,linear_worsttime,"o", label="Worst Case of Linear Search")
 plt1.set_title("Linear Search")
 plt1.set_xlabel("Length of Array")
-plt1.set_ylabel("Time")
+plt1.set_ylabel("Time (in s)")
 
 plt1.legend()
 
@@ -44,13 +44,13 @@ plt2.plot(sizes,binary_besttime,"*", label="Best Case of Binary Search")
 for size in sizes:
     data =range(size)
     start = time.time()
-    binary_search(data,0,size-1,0)
+    binary_search(data,0,size-1,-1)
     end = time.time()
     binary_worsttime.append(end-start)
 plt2.plot(sizes,binary_worsttime,"o", label="Worst Case of Binary Search")
 plt2.set_title("Binary Search")
 plt2.set_xlabel("Length of Array")
-plt2.set_ylabel("Time")
+plt2.set_ylabel("Time (in s)")
 
 plt2.legend()
     
