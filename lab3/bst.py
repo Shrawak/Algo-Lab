9e7ab70a74336088daae1f9e2832eef7eee8ca66
ShrawakB <sb02431318@student.ku.edu.np>
2022-04-16 13:10:35 +0545

    lab 3 done

---
+++ lab3/bst.py
@@ -0,0 +1,142 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.LEFT = None
+        self.RIGHT = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.__size = 0
+
+    def size(self):
+        return self.__size
+
+    def add(self, key, value):
+        NewNode = Node(key=key, value=value)
+        if not self.root:
+            self.root = NewNode
+        else:
+            self.__add(self.root, NewNode)
+        self.__size += 1
+
+    def __add(self, root, Node):
+        if not root:
+            return
+
+        if Node.key < root.key:
+            if not root.LEFT:
+                root.LEFT = Node
+                return
+            self.__add(root.LEFT, Node)
+            return
+        else:
+            if not root.RIGHT:
+                root.RIGHT = Node
+                return
+            self.__add(root.RIGHT, Node)
+            return
+
+    def search(self, key):
+        Node = self.__search(self.root, key)
+        if Node:
+            return Node.value
+        return False
+
+    def __search(self, root, key):
+        if not root:
+            return False
+        if key == root.key:
+            return root
+        elif key < root.key:
+            return self.__search(root.LEFT, key)
+        else:
+            return self.__search(root.RIGHT, key)
+
+    def smallest(self):
+        if not self.root:
+            return False
+        SmallestNode = self.__smallest(self.root)
+        return (SmallestNode.key, SmallestNode.value)
+
+    def __smallest(self, root):
+        if not root.LEFT:
+            return root
+        return self.__smallest(root.LEFT)
+
+    def largest(self):
+        if not self.root:
+            return False
+        LargestNode = self.__largest(self.root)
+        return (LargestNode.key, LargestNode.value)
+
+    def __largest(self, root):
+        if not root.RIGHT:
+            return root
+        return self.__largest(root.RIGHT)
+
+    def remove(self, key):
+        if not self.__search(self.root, key):
+            return False
+        self.root = self.__remove(self.root, key)
+        self.__size -= 1
+
+    def __remove(self, root, key):
+        if not root:
+            return root
+        elif key < root.key:
+            root.LEFT = self.__remove(root.LEFT, key)
+            return root
+        elif key > root.key:
+            root.RIGHT = self.__remove(root.RIGHT, key)
+            return root
+        else:
+            if not root.LEFT and not root.RIGHT:
+                return None
+            elif not root.LEFT or not root.RIGHT:
+                if root.LEFT:
+                    return root.LEFT
+                else:
+                    return root.RIGHT
+            else:
+                successor = self.__largest(root.LEFT)
+                root.key, root.value = successor.key, successor.value
+                root.LEFT = self.__remove(root.LEFT, successor.key)
+                return root
+
+    def inorder_walk(self):
+        visited_nodes = []
+        self.__inorder_walk(self.root, visited_nodes)
+        return visited_nodes
+
+    def __inorder_walk(self, root, visited_nodes):
+        if not root:
+            return
+        self.__inorder_walk(root.LEFT, visited_nodes)
+        visited_nodes.append(root.key)
+        self.__inorder_walk(root.RIGHT, visited_nodes)
+
+    def preorder_walk(self):
+        visited_nodes = []
+        self.__preorder_walk(self.root, visited_nodes)
+        return visited_nodes
+
+    def __preorder_walk(self, root, visited_nodes):
+        if not root:
+            return
+        visited_nodes.append(root.key)
+        self.__preorder_walk(root.LEFT, visited_nodes)
+        self.__preorder_walk(root.RIGHT, visited_nodes)
+
+    def postorder_walk(self):
+        visited_nodes = []
+        self.__postorder_walk(self.root, visited_nodes)
+        return visited_nodes
+
+    def __postorder_walk(self, root, visited_nodes):
+        if not root:
+            return
+        self.__postorder_walk(root.LEFT, visited_nodes)
+        self.__postorder_walk(root.RIGHT, visited_nodes)
+        visited_nodes.append(root.key)
